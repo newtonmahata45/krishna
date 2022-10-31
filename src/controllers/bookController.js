60005547302af2e4f5442ca2d33ece51f7eb0098
newtonmahata45 <newtonmahata45@gmail.com>
2022-10-31 11:23:10 +0530

    populate and reference

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,87 @@
 const authorModel = require("../models/authorModel")
-const bookModel= require("../models/bookModel")
+const bookModel = require("../models/bookModel")
+const publisherModel = require("../models/publisherModel.js")
 
-const createBook= async function (req, res) {
+const createBook = async function (req, res) {
     let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
-}
 
-const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
-    res.send({data: books})
+    let checkAuthorid = await authorModel.find().select({ _id: 1 })
+    // let checkAuthorid = await authorModel.findById({_id:book.author}).select({_id:1});
+
+    let checkPublisherid = await publisherModel.find().select({ _id: 1 })
+    // let checkPublisherid = await publisherModel.findById({_id:book.publisher}).select({_id:1});
+
+
+
+    // console.log(checkAuthorid)
+    if (!book.author) {
+        res.send("Author ID is required")
+    }
+    // for (i = 0; i < checkAuthorid.length; i++) {
+
+    //     if (checkAuthorid[i] != book.author) {
+            
+    //         res.send("Author Id is WORNG!")
+    //     }
+        else if (!book.publisher) {
+            res.send('Publisher Id is required')
+        // } else if (checkPublisherid != book.author) {
+        //     res.send("Publisher Id is WORNG!")
+        }
+
+        else {
+            let bookCreated = await bookModel.create(book)
+            // console.log(checkAuthorid)
+            res.send({ data: bookCreated })
+        }
+    // }
+}
+const putNewBook = async function (req, res) {
+    let Penguin = await publisherModel.findOne({ name: "Penguin" });
+    // let id1 = obj1._id;
+    // console.log(id1);
+    let HarperCollins = await publisherModel.findOne({ name: "HarperCollins" });
+    // let id2 = obj2._id;
+    // console.log(id2);
+
+    let newbooks = await bookModel.updateMany(
+        { publisher: [Penguin, HarperCollins] },
+        { $set: { isHardCover: true } },
+        { new: true }
+    );
+    let updatedbooks = await bookModel.find({ newbooks }).populate('author').populate('publisher');
+    res.send({ data: updatedbooks });
+};
+const updateRating = async function (req, res) {
+    let arr1 = await authorModel.find({ ratings: { $gt: 3.5 } });
+    let newarr = [];
+    for (i of arr1) {
+        id = i._id;
+        let tosend = await bookModel.findOneAndUpdate(
+            { author: id },
+            { $inc: { price: 10 } },
+            { new: true }
+        ).populate('author').populate('publisher');
+        newarr.push(tosend);
+    }
+    res.send({ mes: newarr });
+};
+const getBooksData = async function (req, res) {
+    let books = await bookModel.find().populate('author').populate('publisher');
+    res.send({ data: books })
 }
 
 const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
+    let specificBook = await bookModel.find().populate('author')
+    res.send({ data: specificBook })
 
 }
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+module.exports.createBook = createBook
+module.exports.putNewBook = putNewBook
+module.exports.updateRating = updateRating
+
+module.exports.getBooksData = getBooksData
 module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const authorController= require("../controllers/authorController")
-const bookController= require("../controllers/bookController")
+const authorController= require("../controllers/authorController.js")
+const bookController= require("../controllers/bookController.js")
+const publisherController= require("../controllers/publisherController.js")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
 router.post("/createAuthor", authorController.createAuthor  )
+router.post("/createBook", bookController.createBook  )
+router.post("/creatPublisher",publisherController.createPublisher)
 
 router.get("/getAuthorsData", authorController.getAuthorsData)
-
-router.post("/createBook", bookController.createBook  )
-
 router.get("/getBooksData", bookController.getBooksData)
-
 router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+
+router.put("/putNewBook", bookController.putNewBook)
+router.put("/updateRating", bookController.updateRating)
 
 module.exports = router;
