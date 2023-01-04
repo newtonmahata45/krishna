204642c61c306ee87831b6309c2a9fc2dbdbbfa7
newtonmahata45 <newtonmahata45@gmail.com>
2023-01-04 23:03:30 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import './style.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
 
 class App extends React.Component {
   constructor() {
@@ -35,7 +10,6 @@ class App extends React.Component {
     };
   }
   render() {
-    console.log(this.state.input);
     return (
       <div>
         <textarea
--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from 'react'
+
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import UserfromData from './userForm/userform';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 const CurrentTime = () => {
   return (<h4>The Current Time is {new Date().toString().slice(0, 24, "Newton")} </h4>
   )
 };
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(() => {
   root.render(
     <React.StrictMode>
       <CurrentTime />
-      <div >Newton </div>
-      <h2> Hare Krishna </h2>
-      <App />
+     <UserfromData/>
     </React.StrictMode>
   )
-}, 1000)
+}, 1000);
