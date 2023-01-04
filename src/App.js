c40662200905faf4bd8a5313a6002de025f5bf68
newtonmahata45 <newtonmahata45@gmail.com>
2023-01-04 22:09:42 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+// import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      showParagraph: false,
+    };
+  }
+  render() {
+    console.log(this.state.input);
+    return (
+      <div>
+        <textarea
+          value={this.state.input}
+          onChange={(e) => this.setState({ input: e.target.value })}
+        />
+        <br />
+        <button
+          onClick={() => {
+            this.setState({
+              showParagraph: !this.state.showParagraph,
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Display in uppercase
+        </button>
+        {this.state.showParagraph && <p>{this.state.input.toUpperCase()}</p>}
+      </div>
+    );
+  }
 }
 
-export default App;
+// import React, { useState } from "react";
+
+export default App ;
